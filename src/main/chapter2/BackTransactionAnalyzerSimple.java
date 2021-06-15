70da0b59b8de40ab3203c4244577ddd1a2fbf563
LoveAndCode <practice1356@gmail.com>
2021-06-15 14:55:59 +0900

    feat: bank transaction tutorial

--- src/main/chapter2/BackTransactionAnalyzerSimple.java
+++ src/main/chapter2/BackTransactionAnalyzerSimple.java
@@ -4,26 +4,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.time.Month;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BackTransactionAnalyzerSimple {
 	private static final String RESOURCES = "src/main/resources/";
 
 	public static void main(String[] args) throws IOException {
+		final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
 		final Path path = Paths.get(RESOURCES + args[0]);
 		final List<String> lines = Files.readAllLines(path);
-		double total = 0d;
 
-		for (final String line : lines) {
-			final String[] columns = line.split(",");
-			System.out.println(Arrays.toString(columns));
-			final double amount = Double.parseDouble(columns[1]);
-			total += amount;
-		}
+		final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
 
-		System.out.println("The total for all transactions is  = " + total);
+		System.out.println("The total for all transaction is " + calculateTotalAmount(bankTransactions));
+		System.out.println("Transactions in January " + selectInMonth(bankTransactions, Month.JANUARY));
 	}
 
+	public static double calculateTotalAmount(final List<BankTransaction> bankTransactions) {
+		double total = 0d;
+		for (final BankTransaction bankTransaction : bankTransactions) {
+			total += bankTransaction.getAmount();
+		}
+		return total;
+	}
 
+	public static List<BankTransaction> selectInMonth(final List<BankTransaction> bankTransactions, final Month month) {
+		final List<BankTransaction> bankTransactionsInMonth = new ArrayList<>();
+		for (final BankTransaction bankTransaction : bankTransactions) {
+			if (bankTransaction.getDate().getMonth() == month) {
+				bankTransactionsInMonth.add(bankTransaction);
+			}
+		}
+		return bankTransactionsInMonth;
+	}
 }
